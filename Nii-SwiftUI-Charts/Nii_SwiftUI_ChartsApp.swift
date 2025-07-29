115446292b0525c4606c9ddbe134b0cc9c0dac62
Asad Sayeed <22dp2000212@ds.study.iitm.ac.in>
2025-07-29 16:48:41 +0400

    Initial Commit

---
+++ Nii-SwiftUI-Charts/Nii_SwiftUI_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nii_SwiftUI_ChartsApp.swift
+//  Nii-SwiftUI-Charts
+//
+//  Created by Asad Sayeed on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Nii_SwiftUI_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
